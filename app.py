206294e9ff53dbf83ccbe9ba46d6f9c3f04a2682
Tadek <tadek.waluga@gmail.com>
2026-02-24 21:44:23 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,101 @@
+import json
+import streamlit as st
+import pandas as pd  # type: ignore
+from pycaret.clustering import load_model, predict_model  # type: ignore
+import plotly.express as px  # type: ignore
+
+MODEL_NAME = 'welcome_survey_clustering_pipeline_v2'
+
+DATA = 'welcome_survey_simple_v2.csv'
+
+CLUSTER_NAMES_AND_DESCRIPTIONS = 'welcome_survey_cluster_names_and_descriptions_v2.json'
+
+@st.cache_data
+def get_model():
+    model = load_model(MODEL_NAME)
+    return model
+
+@st.cache_data
+def get_cluster_names_and_descriptions():
+    with open(CLUSTER_NAMES_AND_DESCRIPTIONS, 'r', encoding='utf-8') as f:
+        return json.loads(f.read())
+
+@st.cache_data
+def get_all_participants():
+    model = get_model()
+    all_df = pd.read_csv(DATA, sep=';')
+    df_with_clusters = predict_model(model, data=all_df)
+
+    return df_with_clusters
+
+with st.sidebar:
+    st.header("Powiedz nam coś o sobie")
+    st.markdown("Pomożemy Ci znaleźć osoby, które mają podobne zainteresowania")
+    age = st.selectbox("Wiek", ['<18', '25-34', '45-54', '35-44', '18-24', '>=65', '55-64', 'unknown'])
+    edu_level = st.selectbox("Wykształcenie", ['Podstawowe', 'Średnie', 'Wyższe'])
+    fav_animals = st.selectbox("Ulubione zwierzęta", ['Brak ulubionych', 'Psy', 'Koty', 'Inne', 'Koty i Psy'])
+    fav_place = st.selectbox("Ulubione miejsce", ['Nad wodą', 'W lesie', 'W górach', 'Inne'])
+    gender = st.radio("Płeć", ['Mężczyzna', 'Kobieta'])
+
+    person_df = pd.DataFrame([
+        {
+            'age': age,
+            'edu_level': edu_level,
+            'fav_animals': fav_animals,
+            'fav_place': fav_place,
+            'gender': gender
+        }
+    ])
+
+
+model = get_model()
+all_df = get_all_participants()
+cluster_names_and_description = get_cluster_names_and_descriptions()
+
+predict_cluster_id = predict_model(model, data=person_df)['Cluster'].values[0]
+predicted_cluster_data = cluster_names_and_description[predict_cluster_id]
+
+st.header(f"Najbliżej Ci do grupy: {predicted_cluster_data['name']}")
+st.markdown(predicted_cluster_data['description'])
+same_cluster_df = all_df[all_df['Cluster'] == predict_cluster_id]
+st.metric(label="Liczba twoich znajomych", value=len(same_cluster_df))
+
+st.header("Osoby z grupy")
+fig = px.histogram(same_cluster_df.sort_values(by='age'), x='age')
+fig.update_layout(
+    title="Rozkład wieku w grupie",
+    xaxis_title="Wiek",
+    yaxis_title="Liczba osób",
+    legend_title="Płeć")
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.histogram(same_cluster_df, x='edu_level')
+fig.update_layout(
+    title="Rozkład wykształacenia w grupie",
+    xaxis_title="Wykształcenie",
+    yaxis_title="Liczba osób",
+    legend_title="Płeć")
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.histogram(same_cluster_df, x='fav_animals')
+fig.update_layout(
+    title="Rozkład ulubionych zwierząt w grupie",
+    xaxis_title="Ulubione zwierzęta",
+    yaxis_title="Liczba osób",
+    legend_title="Płeć")
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.histogram(same_cluster_df, x='fav_place')
+fig.update_layout(
+    title="Rozkład ulubionych miejsc w grupie",
+    xaxis_title="Ulubione miejsce",
+    yaxis_title="Liczba osób",
+    legend_title="Płeć")
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.histogram(same_cluster_df, x='gender', color='gender')
+fig.update_layout(
+    title="Rozkład płci w grupie",
+    xaxis_title="Płeć",
+    yaxis_title="Liczba osób")
+st.plotly_chart(fig, use_container_width=True)
